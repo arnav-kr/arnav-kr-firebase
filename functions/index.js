03f1e797fb1303d6f28548cd28c21757f7c3c6fa
arnav-kr <72879799+arnav-kr@users.noreply.github.com>
2021-09-11 23:26:45 +0530

    .

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,22 @@
-const functions = require("firebase-functions");
+// const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// // // Create and Deploy Your First Cloud Functions
+// // // https://firebase.google.com/docs/functions/write-firebase-functions
+// //
+// // exports.helloWorld = functions.https.onRequest((request, response) => {
+// //   functions.logger.info("Hello logs!", {structuredData: true});
+// //   response.send("Hello from Firebase!");
+// // });
+
+const functions = require('firebase-functions');
+
+exports.ping = functions.https.onRequest((req, res) => {
+  res.status(200).send(`<!doctype html>
+    <head>
+      <title>Time</title>
+    </head>
+    <body>
+      ${'PONG! Firebase Hosting Nodejs working!'}
+    </body>
+  </html>`);
+});
